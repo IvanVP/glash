a312e9b65933a2fb048b0eff14b06d08661bbd3b
IvanVP <ipetrakov@gmail.com>
2014-05-20 01:13:42 +0400

    Adds Unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,14 +5,15 @@ set :application, "glash"
 set :scm, :git
 set :repository, "git://github.com/IvanVP/glash.git"
 server "glash.ru", :web, :app, :db, :primary => true
-ssh_options[:port] = 30000
-ssh_options[:keys] = "/home/ivan/.ssh/id_rsa.pub"
+set :port, 30000 
+# ssh_options[:port] = 30000
+# ssh_options[:keys] = "/home/ivan/.ssh/id_rsa.pub"
 set :user, "elsant"
 set :group, "elsant"
-set :deploy_to, "/home/elsant/glash"
+set :deploy_to, "/home/#{user}/apps/#{application}"
 set :use_sudo, false
-set :deploy_via, :copy
-set :copy_strategy, :export
+set :deploy_via, :remote_cache
+# set :copy_strategy, :export
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
@@ -30,16 +31,16 @@ set :copy_strategy, :export
 
 
 
-namespace :deploy do
-task :start do ; end
-task :stop do ; end
-desc "Restart the application"
-task :restart, :roles => :app, :except => { :no_release => true } do
-run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-end
-# desc "Copy the database.yml file into the latest release"
-# task :copy_in_database_yml do
-# run "cp #{shared_path}/config/database.yml #{latest_release}/config/"
+# namespace :deploy do
+# task :start do ; end
+# task :stop do ; end
+# desc "Restart the application"
+# task :restart, :roles => :app, :except => { :no_release => true } do
+# run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
 # end
-end
-before "deploy:assets:precompile", "deploy:copy_in_database_yml"
+# # desc "Copy the database.yml file into the latest release"
+# # task :copy_in_database_yml do
+# # run "cp #{shared_path}/config/database.yml #{latest_release}/config/"
+# # end
+# end
+# before "deploy:assets:precompile", "deploy:copy_in_database_yml"
---
+++ config/unicorn.rb
@@ -0,0 +1,24 @@
+root = "/home/elsant/apps/glash/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.glash.sock"
+worker_processes 1
+timeout 30
+
+# Force the bundler gemfile environment variable to
+# reference the capistrano "current" symlink
+before_exec do |_|
+  ENV["BUNDLE_GEMFILE"] = File.join(root, 'Gemfile')
+end
+
+# working_directory File.expand_path("../..", __FILE__)
+# worker_processes 1
+# listen "/home/elsant/glash/tmp/sockets/unicorn.sock"
+# #listen 3000
+# timeout 30
+# pid "/home/elsant/glash/tmp/pids/unicorn_glash.pid"
+# stdout_path "/home/elsant/glash/log/unicorn.log"
+# stderr_path "/home/elsant/glash/log/unicorn.log"
