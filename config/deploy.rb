eaf6778bc91455e065986e0036abf5f5698376ca
IvanVP <ipetrakov@gmail.com>
2014-05-21 01:56:16 +0400

    Adds application.js

--- config/deploy.rb
+++ config/deploy.rb
@@ -49,6 +49,8 @@ namespace :deploy do
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
     run "ln -nfs #{shared_path}/config/application.yml #{release_path}/config/application.yml"
+    run "ln -nfs #{shared_path}/config/initializers/devise.rb #{release_path}/config/initializers/devise.rb"
+    run "ln -nfs #{shared_path}/config/initializers/secret_token.rb #{release_path}/config/initializers/secret_token.rb"
   end
   after "deploy:finalize_update", "deploy:symlink_config"
 
