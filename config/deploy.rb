4c5d7337af0ceae988b07a8de2a50f72314733ad
IvanVP <ipetrakov@gmail.com>
2014-09-10 12:04:07 +0400

    Redesigns greetings page

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,7 @@ set :group, "elsant"
 set :deploy_to, "/home/#{user}/apps/#{application}"
 set :use_sudo, false
 set :deploy_via, :remote_cache
+
 # set :copy_strategy, :export
 
 
@@ -55,29 +56,29 @@ namespace :deploy do
   after "deploy:finalize_update", "deploy:symlink_config"
 
 
-  namespace :assets do
+  # namespace :assets do
 
-    task :precompile, :roles => :web do
-      from = source.next_revision(current_revision)
-      if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
-        run_locally("RAILS_ENV=#{rails_env} rake assets:clean && RAILS_ENV=#{rails_env} rake assets:precompile")
-        run_locally "cd public && tar -jcf assets.tar.bz2 assets"
-        top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
-        run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
-        run_locally "rm public/assets.tar.bz2"
-        run_locally("rake assets:clean")
-      else
-        logger.info "Skipping asset precompilation because there were no asset changes"
-      end
-    end
+  #   task :precompile, :roles => :web do
+  #     from = source.next_revision(current_revision)
+  #     if capture("cd #{latest_release} && #{source.local.log(from)} vendor/assets/ lib/assets/ app/assets/ | wc -l").to_i > 0
+  #       run_locally("RAILS_ENV=#{rails_env} rake assets:clean && RAILS_ENV=#{rails_env} rake assets:precompile")
+  #       run_locally "cd public && tar -jcf assets.tar.bz2 assets"
+  #       top.upload "public/assets.tar.bz2", "#{shared_path}", :via => :scp
+  #       run "cd #{shared_path} && tar -jxf assets.tar.bz2 && rm assets.tar.bz2"
+  #       run_locally "rm public/assets.tar.bz2"
+  #       run_locally("rake assets:clean")
+  #     else
+  #       logger.info "Skipping asset precompilation because there were no asset changes"
+  #     end
+  #   end
 
-    task :symlink, roles: :web do
-      run ("rm -rf #{latest_release}/public/assets &&
-            mkdir -p #{latest_release}/public &&
-            mkdir -p #{shared_path}/assets &&
-            ln -s #{shared_path}/assets #{latest_release}/public/assets")
-    end
-  end
+  #   task :symlink, roles: :web do
+  #     run ("rm -rf #{latest_release}/public/assets &&
+  #           mkdir -p #{latest_release}/public &&
+  #           mkdir -p #{shared_path}/assets &&
+  #           ln -s #{shared_path}/assets #{latest_release}/public/assets")
+  #   end
+  # end
 
 
 
