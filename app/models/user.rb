4bdc5b4c8106b83534bcc6af33619503589fc612
IvanVP <ipetrakov@gmail.com>
2014-10-24 11:44:07 +0400

    Finishes subscribe form

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable, :confirmable,
         :recoverable, :rememberable, :trackable, :validatable
 
-  scope :most_active, -> {order('updated_at DESC').first(9)}
+  scope :most_active, -> {where.not(avatar_file_name: nil).order('updated_at DESC').first(9)}
 
   has_attached_file :avatar, :styles => { :large => "500x500>", :medium => "300x300#", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png", :processors => [:cropper]
 
